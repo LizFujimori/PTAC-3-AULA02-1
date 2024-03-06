2024cdf6e5465f01a26b80ed0708556456730858
LizFujimori <liizfdaan@gmail.com>
2024-03-06 17:16:10 -0400

    footer
    pintado

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import styles from "../components/footer.module.css"
 
 export default function Footer(){
     return(
-        <footer>
-            <p>2024 ''. Todos os direitos reservador</p>
+        <footer className={styles.fix}>
+            <p className={styles.text}>2024 ''. Todos os direitos reservador</p>
             <Image
-                width={26}
-                height={26}
+                width={100}
+                height={40}
                 src={"https://ead.ifms.edu.br/theme/moove/pix/moodle-logo-white.png"}
            />
         </footer>
