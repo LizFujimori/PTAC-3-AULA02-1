0a237f8967f3acdb97c0f0467784601e00fbd344
LizFujimori <liizfdaan@gmail.com>
2024-03-13 17:13:36 -0400

    OPA

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+export default async function Home(){
+  const resposta = await fetch("http://localhost:3000/api",{
+      next: {revalidate: 1}
+ })
 
-export default async function Home() {
-
-  return (
-    <main>
-      <h1>Home</h1>
-    </main>
+const campus = await resposta.json();
+  return(
+      <main>
+          <h1>Home</h1>
+          {
+            campus.map((campi) =>
+              <div>
+                  <p>
+                      {campi.nome_campi}
+                  </p>
+              </div>
+          )};
+      </main>
   )
 }
